e21216c30e2cc695bd09e77b5807ada53b893bab
skywallkee <raduamaistroaie@gmail.com>
2020-01-20 02:21:21 +0200

    Created Platform Table #51

--- SCC/models.py
+++ SCC/models.py
@@ -1,9 +1,15 @@
 from .extensions import db
 
 class User(db.Model):
+    # User login table, unseen by users
     id = db.Column(db.Integer, primary_key=True)
     publicId = db.Column(db.String(64))
     username = db.Column(db.String(35))
     password = db.Column(db.String(64))
     email = db.Column(db.String(64))
     otp_token = db.Column(db.String(64))
+
+class Platform(db.Model):
+    # Partnered platforms available on SCC
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(35), nullable=False)
